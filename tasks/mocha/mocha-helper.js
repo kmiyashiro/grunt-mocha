539256506907632b3926b41faa3ccad0153857a4
Rohan Nicholls <rohan.nicholls@gmail.com>
2012-09-24 22:01:25 +0200

    The Mocha vs mocha test failed, here is a more robust handling of the problem

--- tasks/mocha/mocha-helper.js
+++ tasks/mocha/mocha-helper.js
@@ -1,6 +1,6 @@
 /*
  * Is injected into the spec runner file
- 
+
  * Copyright (c) 2012 Kelly Miyashiro
  * Copyright (c) 2012 "Cowboy" Ben Alman
  * Licensed under the MIT license.
@@ -18,45 +18,53 @@
 
     var GruntReporter = function(runner){
       // 1.4.2 moved reporters to Mocha instead of mocha
-      var reporters = Mocha.reporters || mocha.reporters;
+			// Yuck, yuck, yuck, but at least it runs. The try..catch version is even uglier.
+			var reporters;
+			if(typeof Mocha !== "undefined" && Mocha !== null && Mocha.reporters){
+				reporters = Mocha.reporters;
+			} else if (typeof mocha !== "undefined" && mocha !== null && mocha.reporters){
+				reporters = mocha.reporters;
+			} else {
+					throw(new Error("Can't find mocha, no test lovin' tonight. :("));
+			}
 
       reporters.HTML.call(this, runner);
 
       var stats = this.stats;
-  
+
       runner.on('test', function(test) {
         sendMessage('testStart', test.title);
       });
-  
+
       runner.on('test end', function(test) {
         sendMessage('testDone', test.title, test.state);
       });
-  
+
       runner.on('suite', function(suite) {
         sendMessage('suiteStart', suite.title);
       });
-  
+
       runner.on('suite end', function(suite) {
         if (suite.root) return;
         sendMessage('suiteDone', suite.title);
       });
-  
+
       runner.on('fail', function(test, err) {
         sendMessage('testFail', test.title, err);
       });
-  
+
       runner.on('end', function() {
         var time = new Date() - stats.start;
         time = (time / 1000).toFixed(2);
-    
+
         var failed  = this.failures,
           passed    = this.total - this.failures,
           total     = this.total;
-    
+
         sendMessage('done', failed, passed, total, time);
       });
     };
-    
+
     var phantom = window.PHANTOMJS;
     if (phantom) {
       // Default mocha options
@@ -67,7 +75,7 @@
           },
           options = phantom.options,
           key;
-          
+
       if (options) {
         // If options is a string, assume it is to set the UI (bdd/tdd etc)
         if (typeof options === "string") {
